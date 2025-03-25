8ecc58ba3208203f432d84957a0cbaba2966ce01
senukafer12 <senukafernando1212@gmail.com>
2025-03-25 23:59:35 +0530

    feat: created the basic structure

---
+++ EventTicketingSystem/src/test/java/lk/my/EventTicketingSystem/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.my.EventTicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
